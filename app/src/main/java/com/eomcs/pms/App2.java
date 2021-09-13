0326f7d57e3709da81c3775e19905691227ca21a
ChoiBoBo <ohmyvia12@gmail.com>
2021-09-14 01:04:32 +0900

    .

--- app/src/main/java/com/eomcs/pms/App2.java
+++ app/src/main/java/com/eomcs/pms/App2.java
@@ -10,38 +10,66 @@ public class App2 {
 
     Scanner keyboardScan = new Scanner(System.in);
 
-    System.out.print("번호? ");
-    int no = keyboardScan.nextInt();
-    keyboardScan.nextLine(); // 번호 뒤에 남아 있는 줄바꿈 코드를 제거한다.
+    final int LENGTH = 5; 
 
-    System.out.print("프로젝트명? ");
-    String title = keyboardScan.nextLine();
+    int[] no = new int[LENGTH];
+    String[] title = new String[LENGTH];
+    String[] content = new String[LENGTH];
+    Date[] startDate = new Date[LENGTH];
+    Date[] endDate = new Date[LENGTH];
+    String[] owner = new String[LENGTH];
+    String[] members = new String[LENGTH];
 
-    System.out.print("내용? ");
-    String content = keyboardScan.nextLine();
+    int size = 0;
 
-    System.out.print("시작일? ");
-    Date startDate = Date.valueOf(keyboardScan.nextLine());
+    for(int i = 0; i < LENGTH; i++) {
+      System.out.print("번호? ");
+      no[i] = Integer.valueOf(keyboardScan.nextLine());
 
-    System.out.print("종료일? ");
-    Date endDate = Date.valueOf(keyboardScan.nextLine());
+      System.out.print("프로젝트명? ");
+      title[i] = keyboardScan.nextLine();
 
-    System.out.print("만든이? ");
-    String owner = keyboardScan.nextLine();
+      System.out.print("내용? ");
+      content[i] = keyboardScan.nextLine();
 
-    System.out.print("팀원? ");
-    String members = keyboardScan.nextLine();
+      System.out.print("시작일? ");
+      startDate[i] = Date.valueOf(keyboardScan.nextLine());
+
+      System.out.print("종료일? ");
+      endDate[i] = Date.valueOf(keyboardScan.nextLine());
+
+      System.out.print("만든이? ");
+      owner[i] = keyboardScan.nextLine();
+
+      System.out.print("팀원? ");
+      members[i] = keyboardScan.nextLine();
+
+      size++;
+
+      System.out.print("계속 입력하시겠습니까?(y/N) ");
+      String str = keyboardScan.nextLine();
+      if (!str.equalsIgnoreCase("y")) {
+        break;
+      }
+      System.out.println(); // 빈 줄 출력
+    }
 
     keyboardScan.close();
 
     System.out.println("--------------------------------");
 
-    System.out.printf("번호: %d\n", no);
-    System.out.printf("프로젝트명: %s\n", title);
-    System.out.printf("내용: %s\n", content);
-    System.out.printf("시작일: %s\n", startDate);
-    System.out.printf("종료일: %s\n", endDate);
-    System.out.printf("만든이: %s\n", owner);
-    System.out.printf("팀원: %s\n", members);
+    for(int i = 0; i < size; i++) {
+
+      System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", 
+          no[i],title[i],content[i], startDate[i], endDate[i], owner[i]);
+
+      System.out.printf("번호: %d\n", no[i]);
+      System.out.printf("프로젝트명: %s\n", title[i]);
+      System.out.printf("내용: %s\n", content[i]);
+      System.out.printf("시작일: %s\n", startDate[i]);
+      System.out.printf("종료일: %s\n", endDate[i]);
+      System.out.printf("만든이: %s\n", owner[i]);
+      System.out.printf("팀원: %s\n", members[i]);
+    }
   }
 }
--- app/src/main/java/com/eomcs/pms/App3.java
+++ app/src/main/java/com/eomcs/pms/App3.java
@@ -9,49 +9,69 @@ public class App3 {
     System.out.println("[작업]");
 
     Scanner keyboardScan = new Scanner(System.in);
+    final int LENGTH = 5;
+
+    int[] no = new int[LENGTH];
+    String[] content = new String[LENGTH];
+    Date[] deadline = new Date[LENGTH];
+    int[] status = new int[LENGTH];
+    String[] owner = new String[LENGTH];
+
+    int size = 0;
 
     System.out.print("프로젝트? ");
     String project = keyboardScan.nextLine();
+    System.out.println();
 
-    System.out.print("번호? ");
-    int no = Integer.parseInt(keyboardScan.nextLine());
+    for(int i = 0; i < LENGTH; i++) {
 
-    System.out.print("내용? ");
-    String content = keyboardScan.nextLine();
+      System.out.print("번호? ");
+      no[i] = Integer.parseInt(keyboardScan.nextLine());
 
-    System.out.print("마감일? ");
-    Date deadline = Date.valueOf(keyboardScan.nextLine());
+      System.out.print("내용? ");
+      content[i] = keyboardScan.nextLine();
 
-    System.out.println("상태?");
-    System.out.println("0: 신규");
-    System.out.println("1: 진행중");
-    System.out.println("2: 완료");
-    System.out.print("> ");
-    int status = Integer.valueOf(keyboardScan.nextLine());
+      System.out.print("마감일? ");
+      deadline[i] = Date.valueOf(keyboardScan.nextLine());
 
-    System.out.print("담당자? ");
-    String owner = keyboardScan.nextLine();
+      System.out.println("상태?");
+      System.out.println("0: 신규");
+      System.out.println("1: 진행중");
+      System.out.println("2: 완료");
+      System.out.print("> ");
+      status[i] = Integer.valueOf(keyboardScan.nextLine());
+
+      System.out.print("담당자? ");
+      owner[i] = keyboardScan.nextLine();
+      size++;
+
+      System.out.println("계속 입력하시겠습니까?(y/N)");
+      String input = keyboardScan.nextLine();
+      if(input.equalsIgnoreCase("N") || input.equals("")) {
+        break;
+      }
+      System.out.println();
+    }
 
     keyboardScan.close();
 
     System.out.println("--------------------------------");
 
-    System.out.printf("프로젝트: %s\n", project);
-    System.out.printf("번호: %d\n", no);
-    System.out.printf("내용: %s\n", content);
-    System.out.printf("마감일: %s\n", deadline);
-
-    switch (status) {
-      case 1:
-        System.out.println("상태: 진행중");
-        break;
-      case 2:
-        System.out.println("상태: 완료");
-        break;
-      default:
-        System.out.println("상태: 신규");
+    System.out.println("프로젝트 " + project);
+    for(int i = 0; i < size; i++) {
+      String stateLable = null; 
+      switch (status[i]) {
+        case 1:
+          System.out.println("상태: 진행중");
+          break;
+        case 2:
+          System.out.println("상태: 완료");
+          break;
+        default:
+          System.out.println("상태: 신규");
+      }
+      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+          no[i], content[i], deadline[i], stateLable, owner[i]);
     }
-
-    System.out.printf("담당자: %s\n", owner);
   }
 }
