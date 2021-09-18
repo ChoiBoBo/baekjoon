6531cebbac1256ae96d69f0d8febf21ffbbfe729
ChoiBoBo <ohmyvia12@gmail.com>
2021-09-18 18:19:54 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,72 +1,55 @@
 package com.eomcs.pms;
 
-import java.util.Date;
-import java.util.Scanner;
-
-//1) 배열 사용 전
-//2) 배열 사용 후
-//3) 반복문 적용 : while 문
-//4) 반복문 적용 : for 문
-//5) 여러 문장에서 반복해서 사용하는 값은 변수에 담아서 사용한다.
-//6) 조회용으로만 사용할 변수라면 상수로 선언한다.
-// 7) 
-// 8)
+//1)
+//2)
+//3)
+//4)
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("[회원]");
 
-    final int MAX_LENGTH = 3;
+    while (true) {
+      String input = Prompt.inputString("명령> ");
 
-    int[] no = new int[MAX_LENGTH];
-    String[] name = new String[MAX_LENGTH];
-    String[] email = new String[MAX_LENGTH];
-    String[] password = new String[MAX_LENGTH];
-    String[] photo = new String[MAX_LENGTH]; 
-    String[] tel = new String[MAX_LENGTH];
-    Date[] registeredDate = new Date[MAX_LENGTH];
-
-    Scanner keyboardScan = new Scanner(System.in);
-
-    int size = 0;
-
-    for (int i = 0; i < MAX_LENGTH; i = i + 1) {
-      System.out.print("번호? ");
-      no[i] = Integer.parseInt(keyboardScan.nextLine());
-      System.out.print("이름? ");
-      name[i] = keyboardScan.nextLine();
-      System.out.print("이메일? ");
-      email[i] = keyboardScan.nextLine();
-      System.out.print("암호? ");
-      password[i] = keyboardScan.nextLine();
-      System.out.print("사진? ");
-      photo[i] = keyboardScan.nextLine();
-      System.out.print("전화? ");
-      tel[i] = keyboardScan.nextLine();
-      registeredDate[i] = new Date();
-      size++;
-
-      System.out.println();
-
-
-      System.out.println("계속 입력하시겠습니까? (Y/N)");
-      String input = keyboardScan.nextLine();
-      if(input.equalsIgnoreCase("N") || input.equals("")) {
+      if (input.equals("exit") || input.equals("quit")) {
+        System.out.println("안녕!");
         break;
+      } else if (input.equals("/member/add")) {
+        MemberHandler.add();
+
+      } else if (input.equals("/member/list")) {
+        MemberHandler.list();
+
+      }  else if (input.equals("/project/add")) {
+        ProjectHandler.add();
+
+      }  else if (input.equals("/project/list")) {
+        ProjectHandler.list();
+
+      }  else if (input.equals("/task/add")) {
+        TaskHandler.add();
+
+      }  else if (input.equals("/task/list")) {
+        TaskHandler.list();
+
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
       }
-
+      System.out.println();
     }
-    keyboardScan.close(); // 데이터 입출력이 끝났으면 도구를 닫는다.
 
-    System.out.println("--------------------------------");
-
-    for (int i = 0; i < size; i = i + 1) {
-      System.out.printf("%d, %s, %s, %s, %tY-%5$tm-%5$td\n", 
-          no[i],
-          name[i],
-          email[i],
-          tel[i],
-          registeredDate[i]);
-    }
+    Prompt.close();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
